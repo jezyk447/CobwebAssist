a906147a1744513381bb3a2f897ccfacc0320e3d
-----S <szymkwappp@gmail.com>
2025-06-11 19:42:20 +0200

    Initial commit

---
+++ src/main/java/pl/jezyk_2137/cobwebassist/CobwebAssist.java
@@ -0,0 +1,10 @@
+package pl.jezyk_2137.cobwebassist;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CobwebAssist implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
